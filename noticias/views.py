7df0e77fdea7251e5cc57a2f5bcf3bde827231f2
Rodrigo <rodrigo.torres.escobedo@gmail.com>
2017-06-22 20:14:47 -0400

    nuevos cambios

--- noticias/views.py
+++ noticias/views.py
@@ -7,11 +7,21 @@ from django.template import RequestContext, loader
 
 
 def index(request):
-	data = {}
-	data['object_list'] = Noticia.objects.order_by('-created')[:6]
+	#data = {}
+	#data['object_list'] = Noticia.objects.order_by('-created')[:6]
+	object_list = Noticia.objects.order_by('-created')[:6]
+	#data2 = {}
+	#data['noticia_destacada'] = Noticia.objects.filter(is_destacada=True)[:1]
+	noticia_destacada = Noticia.objects.filter(is_destacada=True).order_by('-created')[:1]
 	template = 'noticias/index.html'
-	return render(request, template, data)
+	return render(request, template, {'object_list':object_list, 'noticia_destacada':noticia_destacada})
 
     #ultima_noticia_list = Categoria.objects.order_by('created')[:5]
     #output = [p.name for p in ultima_noticia_list]
     #return HttpResponse(output)
+
+def detalle_noticia(request, pk):
+    template_name = 'noticias/noticia_detalle.html'
+    noticia_detalle = Noticia.objects.get(pk=pk)
+
+    return render(request, template_name, {'noticia_detalle':noticia_detalle})
